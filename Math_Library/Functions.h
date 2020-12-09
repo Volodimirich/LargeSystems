4cd8741cc73fb9cf61dcc4f504f6734848841c85
Volodimirich <volodimirich@arccn.ru>
2020-12-09 22:18:05 +0300

    Composite function finished

--- Math_Library/Functions.h
+++ Math_Library/Functions.h
@@ -23,9 +23,10 @@ enum Operation {
 
 
 class TFunction {
+public:
     virtual const std::string ToString () = 0;
     virtual double Deriv(double Point) = 0;
-    virtual double operator() (double Point) = 0;
+    virtual double GetVal(double Point) = 0;
 //    virtual ~TFunction() {}
 };
 
@@ -45,7 +46,7 @@ public:
         return var * pow(point, var - 1);
     }
 
-    double operator() (double point) {
+    double GetVal(double point) {
         return pow(point, var);
     }
 };
@@ -66,7 +67,7 @@ public:
         return  pow(var, point) * log(var);
     }
 
-    double operator() (double point) {
+    double GetVal(double point) {
         return pow(var, point);
     }
 };
@@ -107,7 +108,7 @@ public:
         return result;
     }
 
-    double operator() (double point) {
+    double GetVal(double point) {
         double result = 0;
 
         for (int i = 0; i < coefficients.size(); i++)
@@ -121,19 +122,60 @@ class CompFunction : public TFunction{
 private:
     std::shared_ptr<TFunction> function1;
     std::shared_ptr<TFunction> function2;
-    Operation operator_name;
-    std::unordered_map<Operation, std::string> operation_dict = {{ADD,"+"}, {DEL,"-"},
-                                                                 {MUL,"*"}, {DIV, "/"}};
+    std::string operator_name;
+
 public:
-    CompFunction(std::shared_ptr<TFunction> func1, std::shared_ptr<TFunction> func2, Operation oper){
+    CompFunction(std::shared_ptr<TFunction> func1, std::shared_ptr<TFunction> func2, std::string oper){
         function1 = func1;
         function2 = func2;
         operator_name = oper;
     }
     const std::string ToString() {
-        function1->ToString()
+        return function1->ToString() + operator_name + function2->ToString();
+    }
 
+    double Deriv(double point) {
+        if (operator_name == "+") {
+            return function1->Deriv(point) + function2->Deriv(point);
+        }
+        else if (operator_name == "-")
+        {
+            return function1->Deriv(point) - function2->Deriv(point);
+        }
+        else if (operator_name == "*")
+        {
+            return function1->Deriv(point)*function2->GetVal(point) + function1->GetVal(point)*function2->Deriv(point);
+        }
+        else if (operator_name == "/")
+        {
+            return (function1->Deriv(point)*function2->GetVal(point) -
+                    function1->GetVal(point)*function2->Deriv(point)) / pow(function2->GetVal(point), 2);
+        }
+        else {
+            throw std::logic_error("Wrong operation");
+        }
+    }
 
+    double GetVal(double point) {
+        double result = 0;
+        if (operator_name == "+") {
+            return function1->GetVal(point) + function2->GetVal(point);
+        }
+        else if (operator_name == "-")
+        {
+            return function1->GetVal(point) - function2->GetVal(point);
+        }
+        else if (operator_name == "*")
+        {
+            return function2->GetVal(point) * function1->GetVal(point);
+        }
+        else if (operator_name == "/")
+        {
+            return function1->Deriv(point) /  function2->GetVal(point);
+        }
+        else {
+            throw std::logic_error("Wrong operation");
+        }
 };
 
 #endif //MATH_LIBRARY_FUNCTIONS_H
